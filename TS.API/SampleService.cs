6c26f8c79af72ce7382b512d74c5b04f566c893b
Matthew Krizanac <matthewkrizanac@gmail.com>
2015-04-22 13:20:07 -0500

    Added the OWIN service

--- TS.API/SampleService.cs
+++ TS.API/SampleService.cs
@@ -1,12 +1,38 @@
-﻿namespace TS.API
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace TS.API
 {
     public class SampleService
     {
+        protected IDisposable WebApplication;
+
+        /// <summary>
+        /// Launches the WebApplication using Owin
+        /// </summary>
         public void Start()
         {
+            WebApplication = WebApp.Start<WebPipeline>("http://localhost:8080");
         }
+
+        /// <summary>
+        /// Kills the web application if/when the Topshelf service stops
+        /// </summary>
         public void Stop()
         {
+            WebApplication.Dispose();
         } 
+    }
+
+    public class WebPipeline
+    {
+        public void Configuration(IAppBuilder application)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            application.UseWebApi(config);
+        }
     }
 }
